4bd0b5e3bf947c52bf1f1e6cfffc4fad8ade0321
Adrian <hojas.adrian@gmail.com>
2025-10-15 15:38:33 -0400

    add cancel orders

--- src/handlers/ch_cancel.ts
+++ src/handlers/ch_cancel.ts
@@ -2,7 +2,7 @@ import MyContext from "../helpers/context.ts";
 import { gird } from "../aster/grid.ts";
 
 export default async (ctx: MyContext) => {
-  const response = 3; //await gird.cancelAll();
+  const response = await gird.cancelAll();
 
   ctx.reply(`message: ${response}`);
 };
