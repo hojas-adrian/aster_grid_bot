2f5c1169b3fe027f08ae63376ba0f82a95714e8f
Adrian <hojas.adrian@gmail.com>
2025-10-15 20:17:08 -0400

    add start wh

--- src/composer/commands.ts
+++ src/composer/commands.ts
@@ -4,11 +4,13 @@ import MyContext from "../helpers/context.ts";
 import onStart from "../handlers/ch_start.ts";
 import onSet from "../handlers/ch_set.ts";
 import onCancel from "../handlers/ch_cancel.ts";
+import onOn from "../handlers/ch_on.ts";
 
 const composer = new Composer<MyContext>();
 
 composer.command("start", onStart);
 composer.command("set", onSet);
+composer.command("on", onOn);
 composer.command("cancel", onCancel);
 
 export default composer;
